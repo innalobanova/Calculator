86c2f43de73414d6df1d8bf58a806e1a92b421e8
Lobanova Inna <inna.i.lobanova@gmail.com>
2019-11-04 16:32:24 +0100

    improved version for division and multiplication. PC at the cours

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QString>
+#include <QStringList>
+#include <QCompleter>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -18,14 +20,11 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->B8,SIGNAL(clicked(QString)),this,SLOT(digit(QString)));
     connect(ui->B9,SIGNAL(clicked(QString)),this,SLOT(digit(QString)));
     connect(ui->BPLUS,SIGNAL(clicked(bool)),this, SLOT(add()));
-    connect(ui->BDEVIS,SIGNAL(clicked(bool)),this, SLOT(dev()));
-    connect(ui->BMULT,SIGNAL(clicked(bool)),this, SLOT(mul()));
+    connect(ui->BDEVIS,SIGNAL(clicked(bool)),this, SLOT(dev(bool)));
+    connect(ui->BMULT,SIGNAL(clicked(bool)),this, SLOT(mul(bool)));
     connect(ui->BMINUS,SIGNAL(clicked(bool)),this, SLOT(sub()));
     connect(ui->BEGUAL, SIGNAL(clicked(bool)),this,SLOT(equal()));
-
-
-
-
+    connect(ui->actionclose,SIGNAL(triggered(bool)),this,SLOT(close()));
 
 }
 
@@ -35,33 +34,48 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::add()
-{num+=line.toInt();
+{//num+=line.toInt();
 line.clear();
 ui->lineEdit->setText(line);
 o=1;
 }
 
 void MainWindow::sub()
-{num-=line.toInt();
+{//num-=line.toInt();
   line.clear();
  ui->lineEdit->setText(line);
   o=2;
 
 }
 
-void MainWindow::mul()
-{
-    num*=line.toInt();
-    line.clear();
-    ui->lineEdit->setText(line);
-    o=3;
+void MainWindow::mul(bool a)
+{ if(a==false)
+    {o=3;
+        line=ui->lineEdit->text();
+        num1=line.toInt();
+     line.clear();
+     ui->lineEdit->setText(line);}
+  else {o=0;num2 =line.toInt();
+        line.clear();
+        ui->lineEdit->setText(line);
+        result=num1*num2;}
+
+
+
+
 }
 
-void MainWindow::dev()
-{num/=line.toInt();
-    line.clear();
-    ui->lineEdit->setText(line);
-    o=4;
+void MainWindow::dev(bool a)
+{if(a==false)
+    {o=4;line=ui->lineEdit->text();
+        num1=line.toInt();
+     line.clear();
+     ui->lineEdit->setText(line);}
+  else {o=0;num2 =line.toInt();
+        line.clear();
+        ui->lineEdit->setText(line);
+        result=num1/num2;}
+
 
 }
 
@@ -74,13 +88,18 @@ void MainWindow::equal()
 {switch (o) {
     case 1: {add();break;}
     case 2: {sub();break;}
-    case 3: {mul();break;}
-    case 4: {dev();break;}
+    case 3: {mul(true);break;}
+    case 4: {dev(true);break;}
     default: { break;}
     }
-    ui->lineEdit->setText(QString("%1").arg(num));
-    line.clear();
+    ui->lineEdit->setText(QString("%1").arg(result));
+   // line.clear();
+    //numbers<<QString("%1").arg(res);
 
+   //  QCompleter *completer = new QCompleter(numbers);
+//     ui->lineEdit->setCompleter(completer);
+
+   //  num=0;
 
 }
 
@@ -97,6 +116,11 @@ void MainWindow::clearAll()
 void MainWindow::digit(QString p)
 {line.append(p);
  ui->lineEdit->setText(line);
+
+}
+
+void MainWindow::calculation()
+{
 
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <QString>
 #include <QPushButton>
+#include <QStringList>
+#include <QCompleter>
 
 namespace Ui {
 class MainWindow;
@@ -15,9 +17,14 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
-    int num=0;
+    int num1=0;
+    int num2=0;
+    int result=0;
     QString line;
-    QPushButton *b=new QPushButton(this);
+
+    QStringList numbers;
+
+
 
     ~MainWindow();
 
@@ -27,13 +34,15 @@ private:
 private slots:
     void add();
     void sub();
-    void mul();
-    void dev();
+    void mul(bool a);
+    void dev(bool a);
     void point();
     void equal();
     void clear();
     void clearAll();
     void digit(QString);
+    void calculation();
+
 
 
 };
